3be58c82c1e7e7e1640ca6aca417be4199c91e4e
richardlin3 <rslin93@gmail.com>
2015-07-06 16:52:23 +0000

    comments are working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
         redirect_to lesson_path(@lesson)
     end
     
+    def destroy
+        @lesson = Lesson.find(params[:lesson_id])
+        @comment = @lesson.comments.find(params[:id])
+        @comment.destroy
+        redirect_to lesson_path(@lesson)
+    end
+    
     private
     def comment_params
         params.require(:comment).permit(:commenter, :body)
