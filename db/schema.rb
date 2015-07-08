b36a87c0128432c5bdc611a17c0c3977bf41bf0e
Richard Lin <rslin93@gmail.com>
2015-07-08 16:07:25 +0200

    Connection almost working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707125629) do
+ActiveRecord::Schema.define(version: 20150708093319) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -67,6 +67,10 @@ ActiveRecord::Schema.define(version: 20150707125629) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "role"
+    t.string   "publishable_key"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "access_code"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
