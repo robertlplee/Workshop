ada6281d2bbbb69a4021a1ce6740060ce79832db
Robert <robertlplee@gmail.com>
2015-07-10 11:54:38 +0200

    seed profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,72 +10,85 @@ lessons = Lesson.create([
 	{
 		StartTime: DateTime.new(2015,9,1,17,00),
 		EndTime: DateTime.new(2015,9,1,18,00),
-		location: "New York City",
+		location: "73 Kloof St, Gardens, Cape Town, 8001",
 		category: "Arts",
 		name: "Painting",
-		price: 5.00,
-		capacity: 20,
-		description: "Expect nude models",
-		langauge: "body langauge",
+		price: 10.00,
+		capacity: 15,
+		description: "We will have materials available. Spend some time meeting new people and painting their faces!",
+		langauge: "English",
 		host_id: 1
 	}, {
 		StartTime: DateTime.new(2014,9,1,13,00),
 		EndTime: DateTime.new(2015,9,1,15,00),
-		address: "3893 Old Creek Rd Troy MI, 48084",
+		address: "1 Bertha & Jorissen St, Johannesburg, 2000",
 		category: "Sports",
-		name: "stripping",
+		name: "Quidditch",
 		price: 0,
-		capacity: 16,
-		description: "Bring cleats!",
+		capacity: 20,
+		description: "All equipment will be provided, but bring cleats if you have them.",
 		langauge: "English",
 		host_id: 2
 	}, {
 		StartTime: DateTime.new(2014,9,1,13,00),
 		EndTime: DateTime.new(2015,9,1,15,00),
-		location: "Michigan",
-		category: "Sports",
-		name: "Football",
+		location: "Rondebosch, Cape Town, 7700",
+		category: "Education",
+		name: "Cape Townian Apartheid through Literature",
 		price: 0,
 		capacity: 16,
-		description: "Bring cleats!",
-		langauge: "English",
+		description: "Bring something to take notes.",
+		langauge: "English and Afrikaans",
+		host_id: 1
+	},{
+		StartTime: DateTime.new(2014,9,1,13,00),
+		EndTime: DateTime.new(2015,9,1,15,00),
+		location: "19 Park Rd, Cape Town, 8001",
+		category: "Education",
+		name: "Coding",
+		price: 30,
+		capacity: 20,
+		description: "Limitied computers so bring you own if you can.",
+		langauge: "C++",
 		host_id: 2
 	},{
 		StartTime: DateTime.new(2014,9,1,13,00),
 		EndTime: DateTime.new(2015,9,1,15,00),
-		location: "Ohio",
-		category: "Arts",
-		name: "Scupturing",
-		price: 0,
-		capacity: 16,
-		description: "Bring cleats!",
-		langauge: "English",
-		host_id: 2
+		location: "68 Albert Road Woodstock Cape Town 7925",
+		category: "Crafts",
+		name: "Sewing",
+		price: 2,
+		capacity: 12,
+		description: "Everything will be provided!",
+		langauge: "TBD",
+		host_id: 1
 	},{
 		StartTime: DateTime.new(2014,9,1,13,00),
 		EndTime: DateTime.new(2015,9,1,15,00),
-		location: "Cali",
-		category: "Sports",
-		name: "Tennis",
+		location: "31 Orange Street, Gardens, Cape Town",
+		category: "Education",
+		name: "History",
 		price: 0,
 		capacity: 16,
-		description: "Bring cleats!",
-		langauge: "English",
-		host_id: 2
-	},{
-		StartTime: DateTime.new(2014,9,1,13,00),
-		EndTime: DateTime.new(2015,9,1,15,00),
-		location: "Florida",
-		category: "Sports",
-		name: "Basketball",
-		price: 0,
-		capacity: 16,
-		description: "Bring cleats!",
+		description: "Come and learn about world history.",
 		langauge: "English",
 		host_id: 2
 	}
 	])
 
+profile = Profile.create([
+	{
+		name: "Robert Lee",
+		photo: "https://scontent.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/10247417_10153994633535581_1589428203_n.jpg?oh=305bdfab4ff373ba75de5bbf55cef9a8&oe=565493C4",
+		description: "I'm originally from Hong Kong, but am now in the US for college. I love coding!",
+		base: "Providence, Rhode Island",
+		education: "Brown University '17",
+		interests: "Singing, Design, Sociology",
+		user_id: 1
+	}])
 
-
-
+user = User.create([
+	{
+		email: "robertlplee@gmail.com",
+		password: "tagnqe12"
+	}])
